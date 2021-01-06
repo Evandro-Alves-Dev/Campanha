c215648771cc95c3cec73b23911894b79214d9e7
Evandro-Alves-Dev <70543731+Evandro-Alves-Dev@users.noreply.github.com>
2021-01-06 14:23:11 -0300

    Inserção do campo Cliente na tabela para relacionamento

--- time/src/main/java/com/campanha/time/modelo/Campanha.java
+++ time/src/main/java/com/campanha/time/modelo/Campanha.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,13 +14,11 @@ import com.sun.istack.NotNull;
 
 @Entity
 @Table(name = "TB_CAMPANHA")
-public class Campanha {
-	
+public class Campanha {	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
+	private Long id;	
 	
 	@NotNull
 	private String nomeCampanha;
@@ -31,6 +30,9 @@ public class Campanha {
 	@NotNull
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")	
 	private LocalDate dataFimVigencia;
+	
+	//@ManyToOne
+	//private Cliente cliente;
 
 	public Long getId() {
 		return id;
